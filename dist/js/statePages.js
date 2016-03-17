5fa942cbaf968aadaccb1f2bdf7ae702fcca7acd
Tom VanAntwerp <vanantwerp@gmail.com>
2016-03-17 18:04:07 -0400

    Draws basic chart lines

--- dist/js/statePages.js
+++ dist/js/statePages.js
@@ -21,15 +21,18 @@
     initialize: function(summary, brackets) {
       this.summaryData = summary;
       this.bracketsData = brackets;
+      this.keys = ['min', 'firstQuartile', 'median', 'thirdQuartile', 'max'];
 
       this.height = 500;
       this.width = 500;
-      this.x = d3.scale.log().range([0, this.width])
-        .domain(d3.extent(this.summaryData, function(d) { return d[0]; }));
-      this.y = d3.scale.linear().range([this.height, 0])
+      this.x = d3.scale.pow()
+        .rangeRound([0, this.width])
+        .domain(d3.extent(this.summaryData, function(d) { return +d.income; }));
+      this.y = d3.scale.linear()
+        .rangeRound([this.height, 0])
         .domain(
-          [d3.min(this.summaryData, function(d) { return d[1]; }),
-          d3.max(this.summaryData, function(d) { return d[5]; })]);
+          [d3.min(this.summaryData, function(d) { return +d.min; }),
+           d3.max(this.summaryData, function(d) { return +d.max; })]);
       this.xAxis = d3.svg.axis().scale(this.x).orient('bottom');
       this.yAxis = d3.svg.axis().scale(this.y).orient('left');
 
@@ -37,25 +40,29 @@
     },
 
     drawChart: function() {
-      var chartSvg = d3.select('.state-rate-chart');
-      chartSvg.append('svg')
-        .attr('height', this.height)
-        .attr('width', this.width);
+      var chartSvg = d3.select('.state-rate-chart').append('svg');
+      chartSvg
+        .attr('height', chart.height)
+        .attr('width', chart.width);
 
-      for (var i = 0, j = 6; i < j; i++) {
-        this.drawLine(chartSvg, this.summaryData, i);
-      }
+      chart.keys.forEach(function(key) {
+        var data = chart.summaryData.map(function(d) { return {x: d.income, y: d[key]}; });
+        chart.drawLine(chartSvg, data);
+      })
 
       
     },
 
-    drawLine: function(selection, bracketData, statistic) {
+    drawLine: function(selection, bracketData) {
       var theLine = d3.svg.line()
-        .x(function(d) { return chart.x(bracketData[0]); })
-        .y(function(d) { return chart.y(bracketData[statistic]); });
-
+        .x(function(d) { return chart.x(+d.x); })
+        .y(function(d) { return chart.y(+d.y); });
+console.log(chart.x(50000));
         selection.append('path')
           .datum(bracketData)
+          .attr('fill', 'none')
+          .attr('stroke', '#000000')
+          .attr('stroke-width', 1)
           .attr('d', theLine);
     }
   }
